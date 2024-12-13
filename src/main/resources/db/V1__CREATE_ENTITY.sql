61e4aa49e988c10d11d20067775228ecb5f8edad
Alex Kirilin <kirilin.75.ru@gmail.com>
2024-12-13 23:02:58 +0300

    add db for the flyway

---
+++ src/main/resources/db/V1__CREATE_ENTITY.sql
@@ -0,0 +1,71 @@
+create sequence users_sequence
+    start with 1000
+    increment by 1
+    cache 50;
+
+create table users (
+                       id bigint not null default nextval('users_sequence'),
+                       name varchar(40) not null,
+                       lastname varchar(40) not null,
+                       login varchar(40) not null,
+                       password varchar(255) not null,
+                       description text,
+                       photo_url varchar(255),
+    ---------------------------------------
+                       constraint user_id_pk primary key (id),
+                       constraint user_login_uq unique (login)
+);
+
+create sequence team_sequence
+    start with 1000
+    increment by 1
+    cache 50;
+
+create table team (
+                      id bigint not null default nextval('team_sequence'),
+                      name varchar(40) not null,
+                      owner_id bigint not null,
+    -------------------------------------------
+                      constraint team_id_pk primary key (id),
+                      constraint team_name_uq unique (name),
+                      constraint team_owner_id foreign key (owner_id) references users(id) on delete set null
+);
+create sequence project_sequence
+    start with 1000
+    increment by 1
+    cache 50;
+
+create table project (
+                         id bigint not null default nextval('project_sequence'),
+                         name varchar(100) not null,
+                         owner_id bigint not null,
+                         team_id bigint not null,
+                         employee_num integer not null,
+                         description text,
+    ---------------------------------------
+                         constraint project_id_pk primary key (id),
+                         constraint project_name_uq unique (name),
+                         constraint project_team_id_fk foreign key (team_id) references team(id) on delete cascade,
+                         constraint project_owner_id_fk foreign key (owner_id) references users(id) on delete set null
+);
+
+create sequence post_sequence
+    start with 1000
+    increment by 1
+    cache 50;
+
+create table post (
+                      id bigint not null default nextval('post_sequence'),
+                      name varchar(40) not null,
+                      project_id bigint,
+                      author_id bigint,
+                      assignee_id bigint,
+                      description text,
+    ---------------------------------------
+                      constraint post_id_pk primary key (id),
+                      constraint post_name_uq unique (name),
+                      constraint post_project_id_fk foreign key (project_id) references project(id) on delete cascade,
+                      constraint post_author_id_fk foreign key (author_id) references users(id) on delete set null,
+                      constraint post_assignee_id_fk foreign key (assignee_id) references users(id) on delete set null
+)
+
