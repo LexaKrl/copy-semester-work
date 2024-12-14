aaef195aa04508c653c108fefb1c9b3b78923d16
Alex Kirilin <kirilin.75.ru@gmail.com>
2024-12-14 15:08:55 +0300

    modify listener

--- src/main/java/listener/FlywayMigrationListener.java
+++ src/main/java/listener/FlywayMigrationListener.java
@@ -3,21 +3,32 @@ package listener;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
 
+@Slf4j
 @WebListener
 public class FlywayMigrationListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
 
-        String url = "jdbc:postgresql://postgres.railway.internal:5432/railway";
-        String user = "postgres";
-        String password = "QVsWAivPKWkscvEbLuLwbSiDlmZShnGW";
+        try {
+            String PGHOST = System.getenv("PGHOST");
+            String PGPORT = System.getenv("PGPORT");
+            String PGDATABASE = System.getenv("PGDATABASE");
 
-        Flyway flyway = Flyway.configure()
-                .dataSource(url, user, password)
-                .load();
-        flyway.migrate();
+            String url = "jdbc:postgresql://%s:%s/%s".formatted(PGHOST, PGPORT, PGDATABASE);
+
+            String user = System.getenv("PGUSER");;
+            String password = System.getenv("PGPASSWORD");
+
+            Flyway flyway = Flyway.configure()
+                    .dataSource(url, user, password)
+                    .load();
+            flyway.migrate();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
 
     }
 }
