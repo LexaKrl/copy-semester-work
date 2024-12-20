971cae139d1105a62ee575bcdadad027561c228b
Alex Kirilin <kirilin.75.ru@gmail.com>
2024-12-21 01:37:14 +0300

    fix bugs; deploy

--- src/main/java/config/Configuration.java
+++ src/main/java/config/Configuration.java
@@ -1,12 +1,9 @@
 package config;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Optional;
-import java.util.Properties;
 
 public class Configuration {
 
