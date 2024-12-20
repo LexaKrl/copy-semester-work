b700ccadf81df97f60d9ae610343808ca8c4cafc
Alex Kirilin <kirilin.75.ru@gmail.com>
2024-12-21 02:23:07 +0300

    fix bugs; deploy

--- src/main/resources/db/migration/V1__CREATE_ENTITY.sql
+++ src/main/resources/db/migration/V1__CREATE_ENTITY.sql
@@ -1,20 +1,43 @@
-create sequence post_sequence
+create sequence users_sequence
     start with 1000
     increment by 1
     cache 50;
 
-create table post (
-                      id bigint not null default nextval('post_sequence'),
-                      name varchar(40) not null,
-                      project_id bigint,
-                      assignee_id bigint,
-                      description text,
-                      status boolean default false,
-                      photo_url varchar(255),
+create table users (
+                       id bigint not null default nextval('users_sequence'),
+                       name varchar(40) not null,
+                       lastname varchar(40) not null,
+                       login varchar(40) not null,
+                       password varchar(255) not null,
+                       description text,
+                       photo_url varchar(255),
     ---------------------------------------
-                      constraint post_id_pk primary key (id),
-                      constraint post_project_id_fk foreign key (project_id) references project(id) on delete cascade,
-                      constraint post_assignee_id_fk foreign key (assignee_id) references users(id) on delete cascade
+                       constraint user_id_pk primary key (id),
+                       constraint user_login_uq unique (login)
+);
+
+create sequence team_sequence
+    start with 1000
+    increment by 1
+    cache 50;
+
+create table team (
+                      id bigint not null default nextval('team_sequence'),
+                      name varchar(40) not null,
+                      owner_id bigint not null,
+                      password varchar(255) not null,
+    -------------------------------------------
+                      constraint team_id_pk primary key (id),
+                      constraint team_owner_id foreign key (owner_id) references users(id) on delete cascade
+);
+
+create table team_user (
+                           user_id bigint not null,
+                           team_id bigint not null,
+    ------------------------------
+                           constraint user_id_fk foreign key (user_id) references users(id) on delete cascade,
+                           constraint team_id_fk foreign key (team_id) references team(id) on delete cascade,
+                           constraint team_user_pk primary key (user_id, team_id)
 );
 
 create sequence project_sequence
@@ -43,45 +66,27 @@ create table user_project (
                               constraint user_id_fk foreign key (user_id) references users(id) on delete cascade
 );
 
-create sequence team_sequence
+
+create sequence post_sequence
     start with 1000
     increment by 1
     cache 50;
 
-create table team (
-                      id bigint not null default nextval('team_sequence'),
+create table post (
+                      id bigint not null default nextval('post_sequence'),
                       name varchar(40) not null,
-                      owner_id bigint not null,
-                      password varchar(255) not null,
-    -------------------------------------------
-                      constraint team_id_pk primary key (id),
-                      constraint team_owner_id foreign key (owner_id) references users(id) on delete cascade
-);
-
-create table team_user (
-                           user_id bigint not null,
-                           team_id bigint not null,
-    ------------------------------
-                           constraint user_id_fk foreign key (user_id) references users(id) on delete cascade,
-                           constraint team_id_fk foreign key (team_id) references team(id) on delete cascade,
-                           constraint team_user_pk primary key (user_id, team_id)
-);
-
-create sequence users_sequence
-    start with 1000
-    increment by 1
-    cache 50;
-
-create table users (
-                       id bigint not null default nextval('users_sequence'),
-                       name varchar(40) not null,
-                       lastname varchar(40) not null,
-                       login varchar(40) not null,
-                       password varchar(255) not null,
-                       description text,
-                       photo_url varchar(255),
+                      project_id bigint,
+                      assignee_id bigint,
+                      description text,
+                      status boolean default false,
+                      photo_url varchar(255),
     ---------------------------------------
-                       constraint user_id_pk primary key (id),
-                       constraint user_login_uq unique (login)
+                      constraint post_id_pk primary key (id),
+                      constraint post_project_id_fk foreign key (project_id) references project(id) on delete cascade,
+                      constraint post_assignee_id_fk foreign key (assignee_id) references users(id) on delete cascade
 );
+
+
+
+
 
